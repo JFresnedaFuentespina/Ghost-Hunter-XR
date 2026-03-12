f19ca57dfd90fed3f800a3acade774078646590b
JFresnedaFuentespina <jfresnedafuentespina@gmail.com>
2026-03-12 11:06:09 +0100

    disparar laser, generar fantasmas

---
+++ Assets/Scripts/GhostSpawner.cs
@@ -0,0 +1,57 @@
+using System.Collections;
+using Meta.XR.MRUtilityKit;
+using UnityEngine;
+
+public class GhostSpawner : MonoBehaviour
+{
+    public GameObject ghostPrefab;
+    public float spawnTimer = 1f;
+
+    public float minEdgeDistance = 0.3f;
+    public MRUKAnchor.SceneLabels spawnLabels;
+    public float normalOffset;
+    public int spawnTry = 10;
+
+    private float timer;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        timer += Time.deltaTime;
+        if(timer > spawnTimer)
+        {
+            SpawnGhost();
+            timer = 0f;
+        }
+    }
+
+    void SpawnGhost()
+    {
+        MRUKRoom room = MRUK.Instance.GetCurrentRoom();
+
+        int currentTry = 0;
+
+        while (currentTry < spawnTry)
+        {
+            bool hasFoundPosition = room.GenerateRandomPositionOnSurface(MRUK.SurfaceType.VERTICAL, minEdgeDistance, LabelFilter.Included(spawnLabels), out Vector3 pos, out Vector3 norm);
+            if (hasFoundPosition)
+            {
+                Vector3 randomPositionNormalOffset = pos + norm * normalOffset;
+                randomPositionNormalOffset.y = 0;
+
+                Instantiate(ghostPrefab, randomPositionNormalOffset, Quaternion.identity);
+                return;
+            }
+            else
+            {
+                currentTry++;
+            }
+        }
+
+    }
+}
--- Assets/Scripts/Shoot.cs
+++ Assets/Scripts/Shoot.cs
@@ -3,10 +3,14 @@ using UnityEngine;
 public class Shoot : MonoBehaviour
 {
     public GameObject projectilePrefab;
+    public GameObject rayImpactPrefab;
     public Transform spawnPoint;
-    private float bulletSpeed = 10f;
+    public float bulletSpeed = 10f;
+    public float maxDistance = 5f;
     private OVRInput.Controller controller = OVRInput.Controller.RTouch; // Right-hand controller
     private OVRInput.Button button = OVRInput.Button.PrimaryIndexTrigger; // Index trigger button
+    public LayerMask layerMask;
+    public LineRenderer linePrefab;
 
     // Update is called once per frame
     void Update()
@@ -14,7 +18,8 @@ public class Shoot : MonoBehaviour
         bool buttonPressed = OVRInput.GetDown(button, controller);
         if (buttonPressed)
         {
-            ShootBullet();
+            // ShootBullet();
+            ShootRay();
         }
     }
 
@@ -24,5 +29,31 @@ public class Shoot : MonoBehaviour
         Rigidbody rb = projectile.GetComponent<Rigidbody>();
         rb.AddForce(spawnPoint.forward * bulletSpeed, ForceMode.Impulse);
         Destroy(projectile, 5f); // Destroy the projectile after 5 seconds
+    }
+
+    void ShootRay()
+    {
+        Ray ray = new Ray(spawnPoint.position, spawnPoint.forward);
+        bool hasHit = Physics.Raycast(ray, out RaycastHit hit, maxDistance, layerMask);
+        Vector3 endpoint = Vector3.zero;
+        if (hasHit)
+        {
+            endpoint = hit.point;
+            Quaternion rayImpactRotation = Quaternion.LookRotation(-hit.normal);
+            GameObject rayImpact = Instantiate(rayImpactPrefab, hit.point, rayImpactRotation);
+            Destroy(rayImpact, 1f);
+        }
+        else
+        {
+            endpoint = spawnPoint.position + spawnPoint.forward * maxDistance;
+        }
+        
+        LineRenderer line = Instantiate(linePrefab);
+        line.positionCount = 2;
+        line.SetPosition(0, spawnPoint.position);
+        line.SetPosition(1, endpoint);
+
+
+        Destroy(line.gameObject, 10f);
     }
 }
